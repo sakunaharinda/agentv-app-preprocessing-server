c3a67e57cb6213e726499ba166beb2f280ff2270
Sakuna Jayasundara <sakunaj1996@gmail.com>
2025-04-14 22:50:16 +0000

    Added the docker files

--- docker_build_run.sh
+++ docker_build_run.sh
@@ -1,2 +1,3 @@
-docker build -t preprocessing_server .
-docker run -d --name preprocessing_server -p 8000:8000 preprocessing_server
+docker build -t agentv-app-preprocessing-server . # Build the image
+docker run -d --name agentv-app-preprocessing-server -p 8000:8000 agentv-app-preprocessing-server # Run the container
+docker logs -f agentv-app-preprocessing-server # Watch logs
